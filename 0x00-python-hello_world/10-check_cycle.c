4f0dbc3d6d26c889bed62bb368f6afa83a98f82e
Loaielsamra <loaielsamra@gmail.com>
2023-08-10 15:08:40 -0700

    10th 1st

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,26 @@
+#include "lists.h"
 
+/**
+ * check_cycle - checks for cycle in linked list
+ * @list: linked list
+ * Return: 0 if there is no cycle, 1 if there is acycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tmp, *head;
+
+	tmp = list;
+	head = list;
+
+	while (head->next != NULL && tmp != NULL && head->next->next != NULL)
+	{
+		head = head->next->next;
+		tmp = tmp->next;
+
+		if (head == tmp)
+			return (1);
+	}
+
+	return (0);
+}
