250eaf20e5d4e43581769d1b83536b890cc129db
Loaielsamra <loaielsamra@gmail.com>
2023-08-14 13:31:02 -0700

    0th 2nd

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list = []):
+def print_list_integer(my_list=[]):
     if my_list:
-        my_list.reverse
-        for i in my_list:
-            print("{:d}".format(i))
+        for e in my_list:
+            print("{:d}".format(e))
