2f1d0926e0f7039e9153ab5110f2a93cde2bfc70
Loaielsamra <loaielsamra@gmail.com>
2023-08-16 08:40:43 -0700

    4th 2nd

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    copy_set_1 = set_1.difference(set_2)
-    copy_set_2 = set_2.difference(set_1)
-    newset = copy_set_1.union(copy_set_2)
-    return newset
+    return set_1 ^ set_2
