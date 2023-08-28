e7bb662120961344d41d21b618c6863969967473
Loaielsamra <loaielsamra@gmail.com>
2023-08-28 11:18:34 -0700

    fixed pycodestyles for 0th, 1st, and 3rd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
                 print(j, end="")
                 counter += 1
     except IndexError:
-            pass
+        pass
     print()
     return counter
-
