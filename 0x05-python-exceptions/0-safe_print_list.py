5f3c54a37d6d6ce14bfdff5288ecf259da4cdd1b
Loaielsamra <loaielsamra@gmail.com>
2023-08-28 03:08:30 -0700

    0th 1st

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        for i, j in enumerate(my_list):
+            if (i < x):
+                print(j, end="")
+                counter += 1
+    except IndexError:
+            pass
+    print()
+    return counter
+
