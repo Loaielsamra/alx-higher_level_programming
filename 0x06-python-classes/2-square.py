3fe655fc50ddfa8fa266e5e3217c22bc755d5b3b
Loaielsamra <loaielsamra@gmail.com>
2023-08-29 10:04:41 -0700

    2nd 1st

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+    This is a module for a square
+    '''
+
+
+class Square:
+    '''
+        The class square itself
+        '''
+    def __init__(self, size=0):
+        '''
+            This sets the size attribute
+            '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
