b95eec58f76eea5608d52522e960eff1b9cecd53
Loaielsamra <loaielsamra@gmail.com>
2023-08-29 05:18:43 -0700

    1st 1st didnt write doc for the init function

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    This is a module for a square
+    '''
+
+
+class Square:
+    '''
+        The class square itself
+        '''
+    def __init__(self, s):
+        self.__size = s
+
+    pass
