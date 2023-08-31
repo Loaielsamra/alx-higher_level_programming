708007dca5bfadbb0278b24e5511b2f93951c286
Loaielsamra <loaielsamra@gmail.com>
2023-08-31 14:48:44 -0700

    0th 1st

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    Adds to integers
+    '''
+def add_integer(a, b=89):
+    '''
+        Adds 2 integers
+        '''
+    if not isinstance(a, float) and not isinstance(a, int):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, float) and not isinstance(b, int):
+        raise TypeError("b must be an integer")
+
+    return a+b
