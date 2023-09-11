579524061b6bf2fefffe2b7c764849a2f38c9fce
Loaielsamra <loaielsamra@gmail.com>
2023-09-11 15:26:41 -0700

    forgot to import basegeometry module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Module containing Rectangle class"""
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle classs that inherits from BaseGeometry class"""
     def __init__(self, width, height):
         """initializes width and height values as long as they are +ve"""
-        self.integervalidator("width", width)
-        self.integervalidator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
