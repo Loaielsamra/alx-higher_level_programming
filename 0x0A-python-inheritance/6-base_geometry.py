a88695be5fdb620bf643cee0d136b931c9670ee0
Loaielsamra <loaielsamra@gmail.com>
2023-09-11 15:03:53 -0700

    6th 1st

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module containing BaseGeometry class"""
+
+
+class BaseGeometry:
+    """The class itself"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        """ Raises an Exception with message"""
+        raise Exception("area() is not implemented")
