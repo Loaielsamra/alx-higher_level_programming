532e150ed5ca6d3deab562f90b2d19918cff9793
Loaielsamra <loaielsamra@gmail.com>
2023-09-12 11:47:33 -0700

    1st last try

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """writes `text` into `filename`"""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
