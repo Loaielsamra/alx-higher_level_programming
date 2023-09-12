8efa4ab0283ff24748382319b138d529340dc4a2
Loaielsamra <loaielsamra@gmail.com>
2023-09-12 11:27:55 -0700

    0th 5th

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads file `filename` and prints it to standard ouput"""
-    with open(filename, encoding="utf-8") in f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
