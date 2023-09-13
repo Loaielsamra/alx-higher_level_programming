9f28b17387ba9b5d676afe0ade2ba541d5053726
Loaielsamra <loaielsamra@gmail.com>
2023-09-13 07:52:44 -0700

    8th 1st

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module containing class_to_json function"""
+
+
+def class_to_json(obj):
+    """returns dictionary description with simple data structure
+    for JSON serialization of an object"""
+
+    return obj.__dict__
