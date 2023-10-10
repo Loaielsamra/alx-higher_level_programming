c13973702fe77dc6f9ac4da5223da0e44557d021
Loaielsamra <loaielsamra@gmail.com>
2023-10-10 03:22:41 -0700

    3rd 1st

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/sur/bin/python3
+"""Module containing to_json_string function"""
+import json
+
+
+def to_json_string(obj):
+    """returns JSON representation of string"""
+
+    return (json.dumps(obj))
