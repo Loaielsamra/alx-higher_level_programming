45188051ae93c1ee5d46fc7b8c436427b03c5110
Loaielsamra <loaielsamra@gmail.com>
2023-11-14 14:09:59 -0800

    finished all mandatory

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of recrods with the same score
-SELECT score, COUNT(*) FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) AS "number" FROM second_table GROUP BY score ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all rows in second_table where name is not null
-SELECT socre, name FROM second_table WHERE name != "" ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;
