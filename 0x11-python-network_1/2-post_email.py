122127800f282cdd0cd33c10901ef9e756cd54b2
Loaielsamra <loaielsamra@.com>
2024-01-07 07:35:21 -0800

    fixed pycodestyle for 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """that takes in a URL and an email, sends a POST request to the
-passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
 
 if __name__ == "__main__":
     from urllib import request
     from urllib import parse
     import sys
-    
-    values = {"email":sys.argv[2]}
+
+    values = {"email": sys.argv[2]}
     data = parse.urlencode(values).encode('ascii')
 
     req = request.Request(sys.argv[1], data)
