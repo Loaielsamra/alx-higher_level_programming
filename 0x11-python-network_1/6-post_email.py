6b2041f01fe1d79a4c2726b3980bf38c007c171a
Loaielsamra <loaielsamra@gmail.com>
2024-01-08 00:59:43 +0200

    6th 1st

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+if _name__ == "__main__":
+    import sys
+    import requests
+
+    data = {"email": sys.argv[2]}
+    resp = responses.post(sys.argv[1], params=data)
+    print(resp.text)
