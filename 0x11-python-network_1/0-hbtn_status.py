f4b82884662dccf169ce3aabd0a4450eb32912db
Loaielsamra <loaielsamra@gmail.com>
2024-01-07 14:25:37 +0200

    0th 1st

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__=="__main__":
+    from urllib import request
+
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as fetched:
+        content = fetched.read()
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
