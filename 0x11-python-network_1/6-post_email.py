35d1cf87b906a69e06e0bb6527bbb3a52dcef47b
Loaielsamra <loaielsamra@gmail.com>
2024-01-08 01:02:54 +0200

    6th 2nd forgot 1st undersocre before _name__

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,7 +4,7 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 
-if _name__ == "__main__":
+if __name__ == "__main__":
     import sys
     import requests
 
