9c1952ca60795a8a942f6fc4dbdb9d4b3dce2b84
Loaielsamra <loaielsamra@gmail.com>
2024-01-07 23:34:52 +0200

    fixed pycodestyle for 7th

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the body of the response.
-if the HTTP status code is greater than or equal to 400, prints the value of the
+"""takes in a URL, sends a request to the URL
+and displays the body of the response. if the HTTP
+status code is greater than or equal to 400, prints the value of the
 HTTP status code"""
 
 if __name__ == "__main__":
